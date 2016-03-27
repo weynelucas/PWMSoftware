74b44c93cc38f0830d760c66339f47d2b646dab0
Lucas Weyne <weynelucas@gmail.com>
2016-03-27 10:55:21 -0300

    Optimize library and config enum constants on parameters

--- Source Code/PWMSoftware.c
+++ Source Code/PWMSoftware.c
@@ -1,15 +1,17 @@
 // PWMSoftware.c
 // Runs on LM4F120/TM4C123
 // A library to perform a software PWM on ARM Cortex-M4 Microncontroller (TM4C123 - Tiva C Series Launchpad)
+// Repository 
+// 		https://github.com/weynelucas/PWMSoftware
 // Lucas Weyne 
 //		weynelucas@gmail.com
-//		https://gitlab.com/u/weynelucas
 // 		https://github.com/weynelucas
 // March 13, 2016
 
 /* 
 	Specifications:
 		* The port must be initialized and PWM pin congigured as output before initialize this library
+		* Use enum constants defined on header file to select wich pin you want to use
 		* Using this library, you cannot use SysTick Timer in your project because is used by PWMSoftware
 		* Periodic interrupts has priority 2
 	
@@ -17,24 +19,95 @@
 		* Valvano, Jonathan W. Embedded Systems: Introduction to Arm Cortex-M Microcontrollers.  5th Edition, 2014.
 */
 
-#include "tm4c123gh6pm.h"
 #include "PWMSoftware.h"
 
-Port PWMPin;								// struct to represent pin to perform PWM
+// ***********************************************************************
+//	SysTick registers
+// ***********************************************************************
+#define NVIC_ST_CTRL_R          (*((volatile unsigned long *)0xE000E010))
+#define NVIC_ST_RELOAD_R        (*((volatile unsigned long *)0xE000E014))
+#define NVIC_ST_CURRENT_R       (*((volatile unsigned long *)0xE000E018))
+#define NVIC_SYS_PRI3_R         (*((volatile unsigned long *)0xE000ED20))
+#define NVIC_ST_CTRL_COUNT      0x00010000  // Count flag
+#define NVIC_ST_CTRL_CLK_SRC    0x00000004  // Clock Source
+#define NVIC_ST_CTRL_INTEN      0x00000002  // Interrupt enable
+#define NVIC_ST_CTRL_ENABLE     0x00000001  // Counter mode
+#define NVIC_ST_RELOAD_M        0x00FFFFFF  // Counter load value
+
+
+// ***********************************************************************
+//	GPIO ports (base address)
+// ***********************************************************************
+#define GPIO_PORTA_BASE		0x40004000
+#define GPIO_PORTB_BASE		0x40005000
+#define GPIO_PORTC_BASE		0x40006000
+#define GPIO_PORTD_BASE		0x40007000
+#define GPIO_PORTE_BASE		0x40024000
+#define GPIO_PORTF_BASE		0x40025000
+
+// ***********************************************************************
+//	GPIO pins offset (bit-specific addressing)
+// ***********************************************************************
+#define BIT0_OFFSET		0x0004
+#define BIT1_OFFSET		0x0008
+#define BIT2_OFFSET		0x0010
+#define BIT3_OFFSET		0x0020
+#define BIT4_OFFSET		0x0040
+#define BIT5_OFFSET		0x0080
+#define BIT6_OFFSET		0x0100
+#define BIT7_OFFSET   0x0200
+
+// ***********************************************************************
+//	Port and pin manipulation (auxiliar macros)
+// ***********************************************************************
+#define GetPinReg(pin) 				((volatile unsigned long *) (PortBaseAddress[(pin&0xF00)>>8] + PinAddressOffset[pin&0x0FF]))
+#define GetPortDataReg(port)	((volatile unsigned long *) (PortBaseAddress[port] + 0x03FC))
+#define GetPinMask(pin)				(1<<(pin&0x0FF))
+
+// Ports base address table
+const unsigned long PortBaseAddress[6] = {
+	GPIO_PORTA_BASE,
+	GPIO_PORTB_BASE,
+	GPIO_PORTC_BASE,
+	GPIO_PORTD_BASE,
+	GPIO_PORTE_BASE,
+	GPIO_PORTF_BASE
+};
+
+// Pins offset address table (bit-specific addressing)
+const unsigned long PinAddressOffset[8] = {
+	BIT0_OFFSET,
+	BIT1_OFFSET,
+	BIT2_OFFSET,
+	BIT3_OFFSET,
+	BIT4_OFFSET,
+	BIT5_OFFSET,
+	BIT6_OFFSET,
+	BIT7_OFFSET
+};
+
+// Struct to represent GPIO pin
+typedef struct{
+	volatile unsigned long *addr;
+	unsigned char mask;
+}GPIOPin;
+
+// Global variables
+GPIOPin PWMPin;							// struct to represent pin to perform PWM
 unsigned long High;					// duration of high phase 
 unsigned long Low;					// duration of low phase    
 unsigned long PWMPeriod;   	// PWM period count (High + Low = PWMPeriod)
 
+
 // **************PWMSoftware_Init************************
 // Initialize library with 0% duty cycle
-// Input:  Base address of GPIO Port
-//				 Mask of the pin to perform the PWM
-//				 Period of PWM (depends of the bus clock)
-//					 		Period = Bus clock frequency / PWM frequency
+// Input: Pin to perform software PWM
+//				Period of PWM (depends of the bus clock)
+//					* Period = Bus clock frequency / PWM frequency
 // Output: None
-void PWMSoftware_Init(volatile unsigned long *GPIO_Port_Base_Addr, unsigned long GPIO_Pin_Mask, unsigned long period){
-	PWMPin.GPIO_Data_Addr = GPIO_Port_Base_Addr;
-	PWMPin.GPIO_Pin_Mask = GPIO_Pin_Mask&0xFF;
+void PWMSoftware_Init(Pin pin, unsigned long period){
+	PWMPin.addr = GetPinReg(pin);
+	PWMPin.mask = GetPinMask(pin);
 	
 	PWMPeriod = period;
 	PWMSoftware_SetDuty(0);			// initialize 0% of duty cycle 	
@@ -67,16 +140,16 @@ void PWMSoftware_SetDuty(unsigned char duty){
 
 void SysTick_Handler(void){
 	// Toggle PWM pin
-	if(*PWMPin.GPIO_Data_Addr&PWMPin.GPIO_Pin_Mask){
+	if(*PWMPin.addr&PWMPin.mask){
 		if(Low){																					
-			*PWMPin.GPIO_Data_Addr &= ~PWMPin.GPIO_Pin_Mask; 	// make PWM pin low
-			NVIC_ST_RELOAD_R = Low - 1;												// reload value for low phase
+			*PWMPin.addr &= ~PWMPin.mask; // make PWM pin low
+			NVIC_ST_RELOAD_R = Low - 1;		// reload value for low phase
 		}
 
 	} else{
 		if(High){
-			*PWMPin.GPIO_Data_Addr |= PWMPin.GPIO_Pin_Mask; 	// make PWM pin high
-			NVIC_ST_RELOAD_R = High - 1;											// reload value for high phase
+			*PWMPin.addr |= PWMPin.mask; 	// make PWM pin high
+			NVIC_ST_RELOAD_R = High - 1;	// reload value for high phase
 		}
 	}
 }
--- Source Code/PWMSoftware.h
+++ Source Code/PWMSoftware.h
@@ -1,15 +1,17 @@
 // PWMSoftware.c
 // Runs on LM4F120/TM4C123
 // A library to perform a software PWM on ARM Cortex-M4 Microncontroller (TM4C123 - Tiva C Series Launchpad)
+// Repository 
+// 		https://github.com/weynelucas/PWMSoftware
 // Lucas Weyne 
 //		weynelucas@gmail.com
-//		https://gitlab.com/u/weynelucas
 // 		https://github.com/weynelucas
 // March 13, 2016
 
 /* 
 	Specifications:
 		* The port must be initialized and PWM pin congigured as output before initialize this library
+		* Use enum constants defined on header file to select wich pin you want to use
 		* Using this library, you cannot use SysTick Timer in your project because is used by PWMSoftware
 		* Periodic interrupts has priority 2
 	
@@ -17,108 +19,35 @@
 		* Valvano, Jonathan W. Embedded Systems: Introduction to Arm Cortex-M Microcontrollers.  5th Edition, 2014.
 */
 
+
 //*****************************************************************************
-//
 // PWM Periods
 // period * (1/Bus Clock Frequency)
-//
 //*****************************************************************************
-
 #define PWM_1KHZ_SYSCLK_80MHZ			80000					// PWM period for 1KHz frequency with 80MHz bus clock
 #define PWM_1KHZ_SYSCLK_50MHZ			50000					// PWM period for 1KHz frequency with 50MHz bus clock
 #define PWM_1KHZ_SYSCLCK_16MHz		16000					// PWM period for 1KHz frequency with 16MHz bus clock
 
-//*****************************************************************************
-//
-// GPIO pins mask (PORTA)
-//
-//*****************************************************************************
-#define PA0					0x01
-#define PA1					0x02
-#define PA2					0x04
-#define PA3					0x08
-#define PA4					0x10
-#define PA5					0x20
-#define PA6					0x40
-#define PA7					0x80
 
-//*****************************************************************************
-//
-// GPIO pins mask (PORTB)
-//
-//*****************************************************************************
-#define PB0					0x01
-#define PB1					0x02
-#define PB2					0x04
-#define PB3					0x08
-#define PB4					0x10
-#define PB5					0x20
-#define PB6					0x40
-#define PB7					0x80
+// Enum constans of GPIO pins
+typedef const enum{
+	PA0 = 0x000, PA1, PA2, PA3, PA4, PA5, PA6, PA7,
+	PB0 = 0x100, PB1, PB2, PB3, PB4, PB5, PB6, PB7,
+	PC0 = 0x200, PC1, PC2, PC3, PC4, PC5, PC6, PC7,
+	PD0 = 0x300, PD1, PD2, PD3, PD4, PD5, PD6, PD7,
+	PE0 = 0x400, PE1, PE2, PE3, PE4, PE5,
+	PF0 = 0x500, PF1, PF2, PF3, PF4
+}Pin;
 
-//*****************************************************************************
-//
-// GPIO pins mask (PORTC)
-//
-//*****************************************************************************
-#define PC4					0x10
-#define PC5					0x20
-#define PC6					0x40
-#define PC7					0x80
-
-//*****************************************************************************
-//
-// GPIO pins mask (PORTD)
-//
-//*****************************************************************************
-#define PD0					0x01
-#define PD1					0x02
-#define PD2					0x04
-#define PD3					0x08
-#define PD4					0x10
-#define PD5					0x20
-#define PD6					0x40
-#define PD7					0x80
-
-//*****************************************************************************
-//
-// GPIO pins mask (PORTE)
-//
-//*****************************************************************************
-#define PE0					0x01
-#define PE1					0x02
-#define PE2					0x04
-#define PE3					0x08
-#define PE4					0x10
-#define PE5					0x20
-
-//*****************************************************************************
-//
-// GPIO pins mask (PORTF)
-//
-//*****************************************************************************
-#define PF0					0x01
-#define PF1					0x02
-#define PF2					0x04
-#define PF3					0x08
-#define PF4					0x10
-
-
-// Struct to represent PWM pin
-typedef struct {
-	volatile unsigned long *GPIO_Data_Addr;			// adress of Port data register 
-	unsigned long GPIO_Pin_Mask;								// mask of pin
-} Port;
 
 
 // **************PWMSoftware_Init************************
-// Initialize library configure periodic interrupt
-// Input:  Base address of GPIO Port
-//				 Mask of the pin to perform the software PWM
-//				 Period of PWM (depends of the bus clock)
-//					 		period = Bus clock frequency / PWM frequency
+// Initialize library with 0% duty cycle
+// Input: Pin to perform software PWM
+//				Period of PWM (depends of the bus clock)
+//					* Period = Bus clock frequency / PWM frequency
 // Output: None
-void PWMSoftware_Init(volatile unsigned long *GPIO_Port_Base_Addr, unsigned long GPIO_Pin_Mask, unsigned long period);
+void PWMSoftware_Init(Pin pin, unsigned long period);
 
 // **************PWMSoftware_SetDuty*********************
 // Configure duty cycle of PWM
